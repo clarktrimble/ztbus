de8e1ca2035477d0d95b8cf749bc3d6cd0eae63c
trimble <trimble@tartu.boxworld.com>
2023-11-27 07:38:25 -0600

    cfg lgr directly

--- etc/ztb-dev.sh
+++ etc/ztb-dev.sh
@@ -3,9 +3,8 @@ export ZTB_CLIENT_BASEURI="https://localhost:9200"
 export ZTB_CLIENT_SKIPVERIFY="true"
 export ZTB_CLIENT_USER="elastic"
 
+export ZTB_LOGGER_MAXLEN=999
 export ZTB_ELASTIC_IDX="ztbus003"
-
 export ZTB_SVC_DRYRUN="true"
 
-export ZTB_TRUNCATE=999
 export ZTB_DATAPATH="test/data/B183_2019-06-24_03-16-13_2019-06-24_18-54-06.csv"
